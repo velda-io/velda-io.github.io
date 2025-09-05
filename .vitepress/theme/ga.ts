abe455220b9a8681898bdbf998404571f7e47cd7
Chuan Qiu <chuanq@velda.io>
2025-09-05 12:25:33 -0700

    Add apollo

--- .vitepress/theme/ga.ts
+++ .vitepress/theme/ga.ts
@@ -17,6 +17,12 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', '${gaId}');`
       document.head.appendChild(inline)
+      const apollo = document.createElement('script')
+      apollo.innerHTML = `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+o.onload=function(){window.trackingFunctions.onLoad({appId:"687810e0a1b376000d52b1f4"})},
+document.head.appendChild(o)}initApollo();`
+      document.head.appendChild(apollo)
     }
     router.onAfterRouteChanged = (to) => {
       if (window.gtag) {
